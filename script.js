2efa82c179b698cfa2958896f6cffe818a6bca55
Gabriel Roldan <roldanbiel@gmail.com>
2023-10-05 17:12:46 -0300

    adding script.js to scroll top smooth

---
+++ script.js
@@ -0,0 +1,35 @@
+var card = document.querySelector('#card1')
+
+card.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+})
+
+var card = document.querySelector("#card2")
+
+card.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  })
+})
+
+var card = document.querySelector("#card3")
+
+card.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  })
+})
+
+var card = document.querySelector("#card4")
+
+card.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  })
+})
