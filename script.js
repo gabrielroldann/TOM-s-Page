b1acb0c66ff731e3f24c1e117f479de4fb1ff570
Gabriel Roldan <roldanbiel@gmail.com>
2023-10-09 13:47:38 -0300

    recreate smooth scroll to top

--- script.js
+++ script.js
@@ -1,35 +1,8 @@
-var card = document.querySelector('#card1')
+var card = document.getElementsById('card')
 
 card.addEventListener("click", function() {
   window.scrollTo({
     top: 0,
     behavior: "smooth"
-  })
-})
-
-var card = document.querySelector("#card2")
-
-card.addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  })
-})
-
-var card = document.querySelector("#card3")
-
-card.addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  })
-})
-
-var card = document.querySelector("#card4")
-
-card.addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
   })
 })
